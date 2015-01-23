511a3cc40b9d956df557133f6ca1a12290ffa2ae
Benjamin Thaut <code@benjamin-thaut.de>
2015-01-23 21:38:45 +0100

    Initialize std streams in DllMain as well.

--- src/rt/dllmain.d
+++ src/rt/dllmain.d
@@ -4,17 +4,22 @@ import core.sys.windows.windows;
 import core.sys.windows.dllfixup;
 import core.stdc.stdio;
 
+debug = PRINTF;
+
 extern (Windows) 
 BOOL DllMain(HINSTANCE hInstance, ULONG ulReason, LPVOID pvReserved) 
 {
   switch(ulReason)
   {
     case DLL_PROCESS_ATTACH:
-      printf("druntime loaded\n");
+      debug(PRINTF) printf("druntime loaded\n");
       _d_dll_fixup();
+      // We need to initialize std streams here as well in case
+      // D is used from a C program so _d_main is never called.
+      _d_init_std_streams(); 
       break;
     case DLL_PROCESS_DETACH:
-      printf("druntime unloaded\n");
+      debug(PRINTF) printf("druntime unloaded\n");
       break;
     default:
       return true;
